941c2790d9e63a1a764d5b58b4e0b458de1c70a5
whirlwin <whirlwin90@gmail.com>
2015-10-31 23:23:31 +0100

    Add .npmignore

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,12 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
 
-gulp.task("default", ["watch"], () => {
+gulp.task("default", () => {
     return gulp.src("./src/**/*.js")
         .pipe(babel({ presets: ["es2015"] }))
         .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("watch", () => {
+gulp.task("dev", ["default"], () => {
     gulp.watch("./src/**/*.js", ["default"]);
 });
